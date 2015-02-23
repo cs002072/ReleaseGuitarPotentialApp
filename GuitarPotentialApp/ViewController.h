bd5098df1d1898603340e1590435a3ab0523aeb4
Shinichiro OKA <cs002072@gmail.com>
2015-02-23 15:39:15 +0900

    Initial Commit

---
+++ GuitarPotentialApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuitarPotentialApp
+//
+//  Created by 岡 慎一郎 on 2015/02/23.
+//  Copyright (c) 2015年 oka.shinichiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GuitarPotentialApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GuitarPotentialApp
+//
+//  Created by 岡 慎一郎 on 2015/02/23.
+//  Copyright (c) 2015年 oka.shinichiro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
