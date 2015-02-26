c9d083f74f4b1b06b6a3320e79bb05d4e029521a
Shinichiro OKA <cs002072@gmail.com>
2015-02-26 14:41:43 +0900

    NSMutableArrayにdictionaryを追加できた！２

--- GuitarPotentialApp/detailViewController.m
+++ GuitarPotentialApp/detailViewController.m
@@ -20,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _songHistoryArray = [[NSMutableArray alloc] init];
     
     /***********************************************/
     /**********テキストファイル読み込みフェーズ***********/
