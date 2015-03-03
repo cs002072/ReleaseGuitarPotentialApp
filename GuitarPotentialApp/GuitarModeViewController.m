2a4b2847139d21901b6ba319c20bd28a8f83c6a1
Shinichiro OKA <cs002072@gmail.com>
2015-03-04 00:16:44 +0900

    指板１つ完成！

--- GuitarPotentialApp/GuitarModeViewController.m
+++ GuitarPotentialApp/GuitarModeViewController.m
@@ -39,16 +39,7 @@
     //画面遷移するViewControllerのカプセル化（インスタンス化）
     GuitarViewController *dvc = [self.storyboard instantiateViewControllerWithIdentifier:@"GuitarViewController"];
     
-    //選択された地域の友達リストを遷移先に渡す
-    //    dvc.friendsList = _areaArray[indexPath.row][@"FriendsList"];
-    //    dvc.areaString = _areaArray[indexPath.row][@"Name"];
-
-    
     dvc.hidesBottomBarWhenPushed = YES;  // 画面遷移後にタブバーを隠すための処理
-//    [self.navigationController pushViewController:dvc animated:YES];
-    
-    
-    
     //ナビゲーションコントローラーの機能で画面遷移
     [[self navigationController] pushViewController:dvc animated:YES];
 }
--- GuitarPotentialApp/GuitarViewController.m
+++ GuitarPotentialApp/GuitarViewController.m
@@ -12,7 +12,10 @@
     NSArray *_codeId;
     NSMutableArray *_pushedFlag;
     NSMutableArray *_DummyPushedFlag;
+    NSMutableArray *_dummyArray;
+    NSMutableArray *_truePosition;
     NSArray *_codeImageView;
+
     BOOL _isflag;
 //    _songList = @[@"涙のキッス", @"いとしのエリー"];
 }
@@ -25,8 +28,8 @@
     [super viewDidLoad];
     
     _pushedFlag = [[NSMutableArray alloc] init];
-    _DummyPushedFlag = [[NSMutableArray alloc] init];
-
+    _dummyArray = [[NSMutableArray alloc] init];
+    
     // Do any additional setup after loading the view.
     _codeId = @[@[@"11", @"21", @"31", @"41", @"51", @"61"],
                 @[@"12", @"22", @"32", @"42", @"52", @"62"],
@@ -35,44 +38,40 @@
                 @[@"15", @"25", @"35", @"45", @"55", @"65"],
                 @[@"16", @"26", @"36", @"46", @"56", @"66"],];
     for (int i = 0; i < 6; i++) {
+        _DummyPushedFlag = [[NSMutableArray alloc] init];
         for (int j = 0; j < 7; j++) {
             _isflag = NO;
-//            _DummyPushedFlag = [_pushedFlag objectAtIndex:i];
             [_DummyPushedFlag addObject:@(_isflag)];
-//            [_DummyPushedFlag set]
-            NSLog(@"dsdfsdf");
         }
-        
+        [_pushedFlag addObject:_DummyPushedFlag];
     }
-//    _pushedFlag = @[@[@NO, @NO, @NO, @NO, @NO, @NO, @NO],
-//                      @[@YES, @NO, @NO, @NO, @NO, @NO, @NO],
-//                      @[@NO, @NO, @NO, @NO, @NO, @NO, @NO],
-//                      @[@NO, @NO, @NO, @NO, @NO, @NO, @NO],
-//                      @[@NO, @NO, @NO, @NO, @NO, @NO, @NO],
-//                      @[@NO, @NO, @NO, @NO, @NO, @NO, @NO]];
     _codeImageView = @[@[self.string11, self.string21, self.string31, self.string41, self.string51, self.string61],
                        @[self.string12, self.string22, self.string32, self.string42, self.string52, self.string62],
                        @[self.string13, self.string23, self.string33, self.string43, self.string53, self.string63],
                        @[self.string14, self.string24, self.string34, self.string44, self.string54, self.string64],
                        @[self.string15, self.string25, self.string35, self.string45, self.string55, self.string65],
                        @[self.string16, self.string26, self.string36, self.string46, self.string56, self.string66]];
-//    NSLog(@"beginTouched");
 }
 
 - (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
-    UIImageView *ImageViewAtGuitar;
+//    UIImageView *ImageViewAtGuitar;
     switch (touch.view.tag) {
+        
         default:
             if ([_pushedFlag[0][1] isEqual:@YES]){
                 self.string11.image = [UIImage imageNamed:@"normal_guitarstring.png"];
+                //pushedFlag[i][j + 1] = false;
+                _isflag = NO;
+                [[_pushedFlag objectAtIndex:0] replaceObjectAtIndex:1 withObject:@(_isflag)];
+                /****************こっから！*****************/
+                //pushedFlag[i][truePosition[i]] = true;
+
                 return;
             }
             self.string11.image = [UIImage imageNamed:@"pushed_guitarstring.png"];
-//            BOOL sdfsd = [
-            
-            //            _pushedFlag[0][1] = false;
-//            _pushedFlag[0][1]=YES;
+            _isflag = YES;
+            [[_pushedFlag objectAtIndex:0] replaceObjectAtIndex:1 withObject:@(_isflag)];
             break;
     }
 }
